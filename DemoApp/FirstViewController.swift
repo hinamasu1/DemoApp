371453d017a2709323236d02ae12b7283a1e1c3d
hinamasu <hinamasu@nssx.co.jp>
2019-07-05 06:09:21 +0900

    First load

---
+++ DemoApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  DemoApp
+//
+//  Created by iiga on 2019/07/05.
+//  Copyright © 2019 hinamasu1. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DemoApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  DemoApp
+//
+//  Created by iiga on 2019/07/05.
+//  Copyright © 2019 hinamasu1. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
